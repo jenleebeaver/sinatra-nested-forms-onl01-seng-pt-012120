f5effacb69c6ff2807dbd8503ecb5f7981bcc787
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-21 17:50:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
+
 require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
     get '/' do
       erb :root
     end
@@ -12,17 +11,16 @@ module FormsLab
       erb :'pirates/new'
     end
 
-     post '/pirates' do
- 		  @pirate = Pirate.new(params[:pirate])
-		 
-   	  params[:pirate][:ships].each do |details|
-		    Ship.new(details)
-		  end
-		 
-		  @ships = Ship.all
-		 
-		  erb :'pirate/show'
-		end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+    
   end
 end
